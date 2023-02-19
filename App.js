8f4d9d00fcbec651b94f8cdf5d0b6f88e70af793
romam20001888 <romanrogankov2012@gmail.com>
2023-02-19 16:28:36 +0300

    adfadfadfadf

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!1231</Text>
+      <Text>Open up App.js to start working on your app!Привет</Text>
       <StatusBar style="auto" />
     </View>
   );
