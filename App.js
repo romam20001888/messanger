261d29f36406544c779871e0af403c35ffba0a81
romam20001888 <romanrogankov2012@gmail.com>
2023-02-19 16:01:13 +0300

    test

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!1</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+git config --global user.email "romanrogankov2012@gmail.com"
+git config --global user.name "romam20001888"
 const styles = StyleSheet.create({
   container: {
     flex: 1,
