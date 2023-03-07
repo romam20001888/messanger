a7ec8b2d16e24fba9621ba33699ffa0648a18962
romam20001888 <romanrogankov2012@gmail.com>
2023-03-07 22:41:55 +0300

    .

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Messanger } from './function/messanger/index';
+import { Messanger } from './function/messanger';
 import * as React from 'react';
 
 export default function App() {
